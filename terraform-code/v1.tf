d14515895880017c1fa654e863c3af7e955af9aa
kiran mh <kiranmh@gmail.com>
2023-09-20 14:30:01 +0530

    jenkins-playbook

--- terraform-code/v1.tf
+++ terraform-code/v1.tf
@@ -28,6 +28,16 @@ resource "aws_security_group" "dev-sg" {
    
   }
 
+  
+  ingress {
+    description      = "Jenkins port 8080"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = [ "0.0.0.0/0" ]
+   
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
